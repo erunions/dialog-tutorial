9fd40fb4fe5c83e60b3faa38c5d4a72d8536702c
erunions <78222901+erunions@users.noreply.github.com>
2022-05-16 14:45:24 -0500

    initial

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const showmodal = document.getElementById('showmodal');
+const modal = document.getElementById('modal');
+
+showmodal.addEventListener('click', function() {
+    modal.open = true;
+});
